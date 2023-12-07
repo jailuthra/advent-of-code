bb9d8f253337ee0a1079143d45f319d2e3febfe2
Jai Luthra <j-luthra@ti.com>
2023-12-07 15:35:23 +0530

    2023: day7 part 1

--- 2023/day7/src/main.rs
+++ 2023/day7/src/main.rs
@@ -1,7 +1,7 @@
 use itertools::Itertools;
-use std::{io::stdin, str::FromStr};
+use std::{collections::HashMap, io::stdin, str::FromStr};
 
-#[derive(Eq, PartialEq, Ord, PartialOrd, Debug)]
+#[derive(Eq, PartialEq, Ord, PartialOrd, Debug, Hash)]
 enum Card {
     One = 1,
     Two,
@@ -19,7 +19,18 @@ enum Card {
     Ace,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum HandType {
+    HighCard = 0,
+    Pair,
+    TwoPair,
+    ThreeKind,
+    FullHouse,
+    FourKind,
+    FiveKind,
+}
+
+#[derive(Debug, PartialEq, Eq)]
 struct Hand {
     cards: [Card; 5],
     bid: u32,
@@ -48,6 +59,65 @@ impl Hand {
             self.cards[i] = c;
         }
     }
+
+    fn calc_type(&self) -> HandType {
+        let mut m: HashMap<&Card, u8> = HashMap::new();
+        for c in &self.cards[..] {
+            match m.get(&c) {
+                Some(count) => {
+                    m.insert(c, count + 1);
+                }
+                None => {
+                    m.insert(c, 1);
+                }
+            }
+        }
+        let sorted = m
+            .into_iter()
+            .sorted_by(|(_, a), (_, b)| b.cmp(a))
+            .collect_vec();
+        match sorted.get(0).unwrap() {
+            (_, 5) => HandType::FiveKind,
+            (_, 4) => HandType::FourKind,
+            (_, 1) => HandType::HighCard,
+            (_, 3) => {
+                if sorted.get(1).unwrap().1 == 2 {
+                    HandType::FullHouse
+                } else {
+                    HandType::ThreeKind
+                }
+            }
+            (_, 2) => {
+                if sorted.get(1).unwrap().1 == 2 {
+                    HandType::TwoPair
+                } else {
+                    HandType::Pair
+                }
+            }
+            _ => panic!("Invalid number of cards"),
+        }
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self.calc_type() != other.calc_type() {
+            self.calc_type().cmp(&other.calc_type())
+        } else {
+            for (s, o) in self.cards.iter().zip_eq(other.cards.iter()) {
+                if s != o {
+                    return s.cmp(o);
+                }
+            }
+            std::cmp::Ordering::Equal
+        }
+    }
 }
 
 impl FromStr for Hand {
@@ -65,8 +135,17 @@ impl FromStr for Hand {
 }
 
 fn main() {
-    let hands: Vec<Hand> = Vec::new();
+    let mut hands: Vec<Hand> = Vec::new();
+    let mut total = 0;
     for line in stdin().lines() {
-        println!("{:?}", Hand::from_str(&line.unwrap()));
+        let h = Hand::from_str(&line.unwrap()).unwrap();
+        hands.push(h);
     }
+    hands.sort();
+    for (i, h) in hands.iter().enumerate() {
+        let winnings = (i + 1) as u32 * h.bid;
+        println!("{:?} winnings {}", h, winnings);
+        total += winnings;
+    }
+    println!("Total {total}");
 }

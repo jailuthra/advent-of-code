f4370b0c1991560fdbd6164b28f5de20721d60ca
Jai Luthra <me@jailuthra.in>
2024-12-01 21:36:57 +0530

    2024: day1 part2
    Signed-off-by: Jai Luthra <me@jailuthra.in>

--- 2024/day1/src/main.rs
+++ 2024/day1/src/main.rs
@@ -1,5 +1,5 @@
 use itertools::{self, zip_eq, Itertools};
-use std::io::stdin;
+use std::{collections::HashMap, io::stdin};
 
 fn main() {
     let mut list1: Vec<u32> = Vec::new();
@@ -12,14 +12,38 @@ fn main() {
         list2.push(b.parse::<u32>().unwrap());
     }
 
+    // PART 1
+
     list1.sort();
     list2.sort();
 
-    let mut total = 0;
+    let mut diff = 0;
 
-    for (a, b) in zip_eq(list1, list2) {
-        total += a.abs_diff(b);
+    for (&a, &b) in zip_eq(&list1, &list2) {
+        diff += a.abs_diff(b);
     }
 
-    println!("{total}");
+    println!("{diff}");
+
+    // PART 2
+
+    let mut map: HashMap<u32, usize> = HashMap::new();
+
+    for b in list2 {
+        if let Some(count) = map.get(&b) {
+            map.insert(b, count + 1);
+        } else {
+            map.insert(b, 1);
+        }
+    }
+
+    let mut similarity = 0;
+
+    for a in list1 {
+        if let Some(count) = map.get(&a) {
+            similarity += a * *count as u32;
+        }
+    }
+
+    println!("{similarity}");
 }
